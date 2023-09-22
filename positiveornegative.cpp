19fe114b6afd2b5a55dbe2af29bd7408a4580834
RavirajSingh2147 <sisodiyaraviraj02@gmail.com>
2023-09-22 11:31:23 +0530

    Full

--- positiveornegative.cpp
+++ positiveornegative.cpp
@@ -1,14 +1,14 @@
+// Using Brute Force
 #include <iostream>
 using namespace std;
-
 int main()
 {
     int n = 10;
     if (n > 0)
     {
-        cout << n << "is Positive" << endl;
+        cout << n << " is Positive" << endl;
     }
-    if (n < 0)
+    else if (n < 0)
     {
         cout << n << " is Negative";
     }
@@ -16,4 +16,45 @@ int main()
     {
         cout << n << " is Zero" << endl;
     }
+}
+
+//  Using Nested if-else Statements
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n = 0;
+    if (n >= 0)
+    {
+        if (n == 0)
+        {
+            cout << n << " is Zero";
+        }
+        else
+        {
+            cout << n << " is Positive" << endl;
+        }
+    }
+    else
+    {
+        cout << n << " is Negative";
+    }
+}
+
+// Using Ternary Operator
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = -90;
+    if (n == 0)
+    {
+        cout <<n<< " is Zero";
+    }
+    else
+    {
+        (n > 0) ? cout << n << " is Positive" : cout << n << " is Nagative";
+    }
+    return 0;
 }
